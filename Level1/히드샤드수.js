6ce8f0fe5e0a0172fc194c426d8502fe9cbbec22
soobbb <soob8437@gmail.com>
2023-05-09 21:12:55 +0900

    히드샤드수

---
+++ Level1/히드샤드수.js
@@ -0,0 +1,10 @@
+function solution(x) {
+  // x = 18
+  var sum = 0;
+  var arr = String(x).split(""); // [1,8]
+
+  for (let i = 0; i < arr.length; i++) {
+    sum += Number(arr[i]); // 1+8
+  }
+  return x % sum == 0 ? true : false;
+}

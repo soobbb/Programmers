aa49a8a6da439f6353c6080a9b32080c825c1c52
soobbb <soob8437@gmail.com>
2023-05-12 00:08:11 +0900

    5.12 Progr

---
+++ Level0/5.11배열 뒤집기.js
@@ -0,0 +1,13 @@
+// function solution(num_list) {
+//   return num_list.reverse()
+// }
+
+function solution(num_list) {
+  return num_list.sort((a, b) => 10);
+}
+
+let num_list2 = [1, 2, 3, 4, 5];
+console.log(solution(num_list2));
+
+let num_list1 = [6, 2, 3, 4, 5];
+console.log(solution(num_list1));

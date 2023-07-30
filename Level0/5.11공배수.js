c062e55306a76471d34866157f0fc3d9b33c0af8
soobbb <jsb8437@naver.com>
2023-07-30 23:58:17 +0900

    서울에서김서방찾기

--- Level0/5.11공배수.js
+++ Level0/5.11공배수.js
@@ -1,9 +1,9 @@
-function solution(number, n, m) {
-  return number % (n * m) === 0 ? 1 : 0;
-}
-function solution(number, n, m) {
-  // 8 8 4
-  if (number % (n * m) === 0 || (number % m === 0 && number % n === 0))
-    return 1;
-  else return 0;
-}
+// function solution(number, n, m) {
+//   return number % (n * m) === 0 ? 1 : 0;
+// }
+// function solution(number, n, m) {
+//   // 8 8 4
+//   if (number % (n * m) === 0 || (number % m === 0 && number % n === 0))
+//     return 1;
+//   else return 0;
+// }
